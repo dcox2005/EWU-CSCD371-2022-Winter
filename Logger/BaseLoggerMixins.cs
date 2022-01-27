bb4a33ab97ae4f271b22712a0ab8e17076aedbcc
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-01-26 16:24:19 -0800

    Updated methods

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -4,27 +4,35 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger? baseLogger, string? message, params object?[] args)
+    public static void Error(this BaseLogger? baseLogger, string message, params object?[] args)
     {
         if(baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
 
+        string? str = String.Format(message, args); 
+        baseLogger.Log(LogLevel.Error, str);
     }
 
-    public static void Warning(this BaseLogger? baseLogger, string? message, params object?[] args)
+    public static void Warning(this BaseLogger? baseLogger, string message, params object?[] args)
     {
         if (baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
 
+        string? str = String.Format(message, args);
+        baseLogger.Log(LogLevel.Warning, str);
     }
 
-    public static void Information(this BaseLogger? baseLogger, string? message, params object?[] args)
+    public static void Information(this BaseLogger? baseLogger, string message, params object?[] args)
     {
         if (baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
 
+        string? str = String.Format(message, args);
+        baseLogger.Log(LogLevel.Information, str);
     }
 
-    public static void Debug(this BaseLogger? baseLogger, string? message, params object?[] args)
+    public static void Debug(this BaseLogger? baseLogger, string message, params object?[] args)
     {
         if (baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
 
+        string? str = String.Format(message, args);
+        baseLogger.Log(LogLevel.Debug, str);
     }
 }
