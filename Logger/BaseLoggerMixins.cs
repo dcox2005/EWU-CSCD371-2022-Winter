1455f837007f06a68e5b97478addc3d8ff6def53
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-01-26 16:04:58 -0800

    added methods

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,30 @@
-﻿namespace Logger;
+﻿using System;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
+    public static void Error(this BaseLogger? baseLogger, string? message, params object?[] args)
+    {
+        if(baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
 
+    }
+
+    public static void Warning(this BaseLogger? baseLogger, string? message, params object?[] args)
+    {
+        if (baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
+
+    }
+
+    public static void Information(this BaseLogger? baseLogger, string? message, params object?[] args)
+    {
+        if (baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
+
+    }
+
+    public static void Debug(this BaseLogger? baseLogger, string? message, params object?[] args)
+    {
+        if (baseLogger is null) throw new ArgumentNullException(nameof(BaseLogger));
+
+    }
 }
