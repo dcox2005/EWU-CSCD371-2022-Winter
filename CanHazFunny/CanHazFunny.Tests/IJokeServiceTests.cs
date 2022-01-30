5f08875cd14abc061c81ab00d865504d77aa948e
David Cox <dcox2005@hotmail.com>
2022-01-30 14:44:11 -0800

    added tests for IJokeService

--- CanHazFunny/CanHazFunny.Tests/IJokeServiceTests.cs
+++ CanHazFunny/CanHazFunny.Tests/IJokeServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CanHazFunny.Tests;
 
@@ -11,4 +12,16 @@ public class IJokeServiceTests
         IJokeService mockJoke = new MockJoke();
         Assert.AreEqual<string>("I have a funny knock knock joke for you", mockJoke.GetJoke());
     }
+
+
+    [TestMethod]
+    public void MyTestMethod()
+    {
+        Mock<IJokeService> mock = new();
+        mock.SetupSequence(funny => funny.GetJoke()).Returns("I have a funny joke for you");
+
+        Assert.AreEqual<string>("I have a funny joke for you", mock.Object.GetJoke());
+    }
+
 }
+
