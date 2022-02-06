646c40856d7930147b064cbe483931508cbfda6d
David Cox <dcox2005@hotmail.com>
2022-02-06 11:04:58 -0800

    created classes and updeted proj files

---
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework.Tests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void NodeCostructor_CreatingANode_Success()
+    {
+        Node myNode = new();
+    }
+}
