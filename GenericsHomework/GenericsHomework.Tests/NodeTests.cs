472c741990a4d92a10115eb60a84f55d5d0aacd9
David Cox <dcox2005@hotmail.com>
2022-02-06 12:34:28 -0800

    Added Node constructor and properties along with ToString. Wrote some tests.

--- GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -11,8 +11,50 @@ namespace GenericsHomework.Tests;
 public class NodeTests
 {
     [TestMethod]
-    public void NodeCostructor_CreatingANode_Success()
+    public void Costructor_CreatingANode_Success()
     {
-        Node myNode = new();
+        int value = 42;
+        Node<int> myNode = new(value);
+        Assert.IsNotNull(myNode);
     }
+
+    [TestMethod]
+    public void Constructor_AcceptingNullValue_Success()
+    {
+        Node<string> myNode = new(null!);
+        Assert.IsNotNull(myNode);
+    }
+
+    [TestMethod]
+    public void ToString_ValueShouldBeReturned_Success()
+    {
+        string value = "some value";
+        Node<string> myNode = new(value);
+        Assert.AreEqual(value, myNode.ToString());
+    }
+
+    [TestMethod]
+    public void ToString_ValueIsNullShouldReturnNull_Succes()
+    {
+        Node<string?> myNode = new(null!);
+        Assert.AreEqual(null, myNode.ToString());
+    }
+
+    [TestMethod]
+    public void ToString_ValueIsNotOfStringTypeReturnsAsString_Success()
+    {
+        int value = 42;
+        Node<int> myNode = new(value);
+        Assert.AreEqual("42", myNode.ToString());
+    }
+
+    [TestMethod]
+    public void Constructor_SettingNextNodeToEqualTheNodeCreated_Succes()
+    {
+        Node<string> myNode = new("value");
+        Assert.AreEqual(myNode, myNode.Next);
+    }
+
+
+
 }
--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace GenericsHomework;
 
-public class Node
+public class Node<TValue>
 {
+    private TValue? Value;
+    public Node<TValue> Next
+    {
+        get;
+        private set;
+    }
+
+    public Node(TValue value)
+    {
+        Next = this;
+        Value = value;
+    }
+
+    public override string? ToString()
+    {
+        if (Value is null)
+        {
+            return null;
+        }
+        else
+        {
+            return Value.ToString();
+        }
+    }
 
 }
