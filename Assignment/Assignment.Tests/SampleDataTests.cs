eafdb675afb26f2d4f87cb8abaf3f255c93fe074
DavidCox <dcox2005@hotmail.com>
2022-02-16 08:03:40 -0800

    created test class and disabled analyzer for now.

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Assignment.Tests;
+
+[TestClass]
+public class SampleDataTests
+{
+
+    [TestMethod]
+    public void CsvRows_RetreivingDatafromCsv_Line1Matches()
+    {
+
+    }
+}
