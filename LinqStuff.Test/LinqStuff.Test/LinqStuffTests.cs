ac7107b2db8a627e6cb8a8fa8fff5695ca46ad7e
DavidCox <dcox2005@hotmail.com>
2022-02-17 17:07:21 -0800

    end of lecture

---
+++ LinqStuff.Test/LinqStuff.Test/LinqStuffTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace LinqStuff.Test;
+
+[TestClass]
+public class LinqStuffTests
+{
+    [TestMethod]
+    public MemberInfo[] getEnumerableMembers()
+    {
+        MemberInfo[] members = typeof(Enumerable).GetMembers();
+        int count = members.Count();
+        Console.WriteLine(count);
+        Assert.AreEqual<int>(count, members.Length);
+        return members;
+    }
+
+    [TestMethod]
+    public void GetAllTheMemberNames()
+    {
+        MemberInfo[] members = getEnumerableMembers();
+        IEnumerable<string> memberNames = members.Select(member => member.Name);
+        Assert.IsTrue(memberNames.Any(name => name == "Contains"));
+        Assert.IsTrue(memberNames.Any(name => name == "Aggregate"));
+        Assert.IsFalse(memberNames.Any(name => name == "Map"));
+        //Assert.IsTrue(memberNames.Contains("Contains"));
+        //Assert.IsTrue(memberNames.Contains("Aggregate"));
+        //Assert.IsFalse(memberNames.Contains("Map"));
+
+        memberNames.Where(name => name == "Contains" || name == "Aggregate");
+
+    }
+    [TestMethod]
+    public void DeferredExecution()
+    {
+        int count = 0;
+        IEnumerable<MemberInfo> memberInfos = getEnumerableMembers().Where(item => item.Name[0] == 'C');
+        IEnumerable<string> memberNames =
+            memberInfos.Select(item => 
+            { 
+                count++; 
+                return item.Name; 
+            });
+
+        Assert.AreEqual<int>(0, count);
+        Assert.AreEqual<int>(7,memberNames.Count());
+        Assert.AreEqual<int>(7, count);
+        int temp = memberNames.Count();
+        Assert.AreEqual<int>(14, count);
+
+    }
+}
---
+++ LinqStuff/LinqStuff/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqStuff;
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
