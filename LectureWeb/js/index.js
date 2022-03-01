9910192caab4a6e39124fabe22610f5edfa24a4d
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-03-01 15:25:25 -0800

    lecture

--- LectureWeb/js/index.js
+++ LectureWeb/js/index.js
@@ -19,3 +19,18 @@ console.log("Hello World: " + person.getName() + " is " + person.age());
 document.getElementById("mybutton").addEventListener("click", function(){
     console.log("Hello World: " + person.getName() + " is " + person.age());
 });
+
+axios({
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=47.658&lon=-117.402&appid=e44ff6715c0c7c9bf43ce3bfe74fdb6f'
+})
+    .then(function (response) {
+        console.log(response);
+        console.log(response.data.main.temp);
+
+        let weatherText = document.querySelector(".weather");
+        weatherText.innerText = "Current Temp: " + response.data.main.temp;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
