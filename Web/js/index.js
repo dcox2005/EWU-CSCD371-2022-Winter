e4bd2a1c362d8d1e9755e9b5f63e298954dba3b3
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-03-03 18:54:23 -0800

    finished menu drop down.

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,29 +1,13 @@
 
-
-function menuDropdown() 
-{
-    // document.addEventListener('click', (e) => {
-    //     if (!e.target.matches('.Menu-button, .Menu-button *')) {
-    //       return;
-    //     }
-    // });
-    document.getElementById("menuDropdown").classList.toggle("Menu-dropdown-show");
-        console.log('It works!');
-}
-
-window.onclick = function(event) 
-{
-    if (!event.target.matches('.Menu-button')) 
-    {
-        var dropdowns = document.getElementsByClassName("Menu-dropdown");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) 
-        {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('Menu-dropdown-show')) 
-            {
-                openDropdown.classList.remove('Menu-dropdown-show');
-            }
-        }
-    }
-}
+let dropdownBtn = document.querySelector('.Menu-button');
+let menuContent = document.querySelector('.Menu-dropdown');
+dropdownBtn.addEventListener('click',()=>{
+   if(menuContent.style.display==="")
+   {
+      menuContent.style.display="block";
+   } 
+   else 
+   {
+      menuContent.style.display="";
+   }
+})
