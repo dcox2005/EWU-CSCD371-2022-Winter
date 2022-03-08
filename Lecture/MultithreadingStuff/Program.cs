0ac8fa42fc35215782d90c0de58ed7da5c71129f
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-03-08 15:45:48 -0800

    commit

--- Lecture/MultithreadingStuff/Program.cs
+++ Lecture/MultithreadingStuff/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        Task task = Task.Run(
+            () => Display('.', cancellationTokenSource.Token)
+            );
+
+        Console.WriteLine("ENTER to exit");
+        Console.ReadLine();
+        cancellationTokenSource.Cancel();
+        task.Wait();
+        Console.WriteLine("Exiting!!!");
+
+    }
+    static void Display(char character, CancellationToken cancellationToken)
+    {
+        while(!cancellationToken.IsCancellationRequested)
+        {
+            Console.Write(character);
+        }
+    }
+
+}
